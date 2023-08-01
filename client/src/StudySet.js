3dca8f57ca3da232b356845369070dfcef2c185f
Jisang Ahn <michahn@umich.edu>
2023-08-01 01:00:25 -0700

    Study Set view added

--- client/src/StudySet.js
+++ client/src/StudySet.js
@@ -9,7 +9,8 @@ const StudySet = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const [ loading, setLoading ] = useState(true);
-    const [ studySet, setStudySet] = useState({});
+    const [ studySetName, setStudySetName] = useState("");
+    const [ termDefs, setTermDefs] = useState([]);
     const [ studySetFound, setStudySetFound ] = useState(true);
     const path_parts = location.pathname.split('/').filter(item => item !== '');
 
@@ -22,7 +23,8 @@ const StudySet = () => {
 
     const studyset_id = path_parts[2]
 
-    const retrieveData = () => {
+
+    useEffect(() => {
         console.log("Retrieving Data")
         if (localStorage.getItem('token') === null) {
             setLoading(false);
@@ -40,19 +42,55 @@ const StudySet = () => {
             })
             .then((data) => {
                 setLoading(false)
-                if (data.hasOwnProperty("message") && data["message"] === "No StudySet found.") {
+                if (data["message"] === "No StudySet found.") {
                     setStudySetFound(false);
                 }
                 else {
                     setStudySetFound(true);
-                    setStudySet(data)
+                    setStudySetName(data["StudySet Name"])
+                    setTermDefs(data["Terms in StudySet"])
                 }
             })
         }
-    }
-    useEffect(() => {
-        retrieveData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps 
     }, [])
+
+    const getTermDefs = () => {
+        if (termDefs.length === 0) {
+            return (
+                <div style={{width: "100%", paddingTop: "2em"}}>
+                <center>
+                This study set has no terms. 
+                </center>
+                </div>
+            )
+        }
+        return (
+            <div className="studySetsBox" style={{alignItems: "center"}}>
+                <h2>Terms in this set</h2>
+                {termDefs.map((termDef, index) => (
+                    <div className="view_only_card" key={index}>
+                        <div className="view_only_card_sub_area"
+                             style={{width: "20%"}}>
+                            {termDef["term"]}
+                        </div>
+                        <div className="view_only_card_sub_area"
+                             style={{width: "60%", borderLeft: "2px solid grey"}}>
+                            {termDef["definition"]}
+                        </div>
+                    </div>  
+                ))}
+            </div>
+        )    
+    }
+
+    if (loading) {
+        return (
+            <>
+                <Navbar logged_in={true} />
+            </>
+        )
+    }
 
     if (!studySetFound) {
         return (
@@ -68,8 +106,25 @@ const StudySet = () => {
     return (
         <>
         <Navbar logged_in={true} />
-        <div className="flush_left_column_page">
-            <h1></h1>
+        <div className="page_content">
+
+            <div style={{paddingBottom: "1em",
+                         borderBottom: "2px solid black",
+                         width: "100%",
+                         display: "flex",
+                         flexDirection: "row",
+                         justifyContent: "space-between",
+                         marginBottom: "2em"
+                        }}>
+                <h1 style={{ "margin": "0", maxWidth: "70%", wordWrap: "break-word", whiteSpace: "normal"}}>{studySetName}</h1>
+                <motion.button className="small_button" whileHover={{scale: 1.1}} style={{height: "2.5em", minWidth: "8em"}}
+                 onClick={() => {}}>
+                    Edit this set
+                </motion.button>
+            </div>
+            
+            {getTermDefs()}
+            
         </div>
         </>
     )
